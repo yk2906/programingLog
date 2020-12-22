69c31d20e64adcca99653ae2f530d330aed2add1
y.kohama <yk050696@gmail.com>
2020-12-22 18:02:53 +0900

    7th commit

--- front/src/components/Home/Status/Tasks.jsx
+++ front/src/components/Home/Status/Tasks.jsx
@@ -1,149 +1,62 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
-import List from '@material-ui/core/List';
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import Checkbox from '@material-ui/core/Checkbox';
-import Button from '@material-ui/core/Button';
-import Divider from '@material-ui/core/Divider';
-import TextField from '@material-ui/core/TextField';
+import React, {useState} from 'react'
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
+    
+    
+const Tasks = () => {
 
-function not(a, b) {
-  return a.filter((value) => b.indexOf(value) === -1);
-}
+  const initialState = {
+    title: '',
+    doing: false,
+  }
 
-function intersection(a, b) {
-  return a.filter((value) => b.indexOf(value) !== -1);
-}
+  const [tasks, setTasks] = useState([initialState])
+  const [taskName, setTaskName] = useState('')
 
-function union(a, b) {
-  return [...a, ...not(b, a)];
-}
+  const inputTaskName = (e) => {
+    setTaskName(e.target.value)
+  }
 
-export default function Tasks() {
-  const [checked, setChecked] = React.useState([]);
-  const [left, setLeft] = React.useState([0, 1, 2, 3]);
-  const [right, setRight] = React.useState([4, 5, 6, 7]);
+  const addTask = () => {
+    setTasks([...tasks, {
+      title: taskName,
+      doing: false,
+    }])
+    setTaskName('')
+  }
 
-  const leftChecked = intersection(checked, left);
-  const rightChecked = intersection(checked, right);
-
-  const handleToggle = (value) => () => {
-    const currentIndex = checked.indexOf(value);
-    const newChecked = [...checked];
-
-    if (currentIndex === -1) {
-      newChecked.push(value);
-    } else {
-      newChecked.splice(currentIndex, 1);
-    }
-
-    setChecked(newChecked);
-  };
-
-  const numberOfChecked = (items) => intersection(checked, items).length;
-
-  const handleToggleAll = (items) => () => {
-    if (numberOfChecked(items) === items.length) {
-      setChecked(not(checked, items));
-    } else {
-      setChecked(union(checked, items));
-    }
-  };
-
-  const handleCheckedRight = () => {
-    setRight(right.concat(leftChecked));
-    setLeft(not(left, leftChecked));
-    setChecked(not(checked, leftChecked));
-  };
-
-  const handleCheckedLeft = () => {
-    setLeft(left.concat(rightChecked));
-    setRight(not(right, rightChecked));
-    setChecked(not(checked, rightChecked));
-  };
-
-  const customList = (title, items) => (
-    <Card>
-      <CardHeader
-        avatar={
-          <Checkbox
-            onClick={handleToggleAll(items)}
-            checked={numberOfChecked(items) === items.length && items.length !== 0}
-            indeterminate={numberOfChecked(items) !== items.length && numberOfChecked(items) !== 0}
-            disabled={items.length === 0}
-            inputProps={{ 'aria-label': 'all items selected' }}
-          />
-        }
-        title={title}
-        subheader={`${numberOfChecked(items)}/${items.length} selected`}
-      />
-      <Divider />
-      <List dense component="div" role="list">
-        {items.map((value) => {
-          const labelId = `transfer-list-all-item-${value}-label`;
-
-          return (
-            <ListItem key={value} role="listitem" button onClick={handleToggle(value)}>
-              <ListItemIcon>
-                <Checkbox
-                  checked={checked.indexOf(value) !== -1}
-                  tabIndex={-1}
-                  disableRipple
-                  inputProps={{ 'aria-labelledby': labelId }}
-                />
-              </ListItemIcon>
-              <ListItemText id={labelId} primary={`List item ${value + 1}`} />
-            </ListItem>
-          );
-        })}
-        <ListItem />
-      </List>
-    </Card>
-  );
-
-  return (
+  return(
     <div>
-      <Grid container spacing={2} justify="center" alignItems="center">
-        <Grid item>{customList('Choices', left)}</Grid>
-        <Grid item>
-          <Grid container direction="column" alignItems="center">
-            <Button
-              variant="outlined"
-              size="small"
-              onClick={handleCheckedRight}
-              disabled={leftChecked.length === 0}
-              aria-label="move selected right"
-            >
-              &gt;
-            </Button>
-            <Button
-              variant="outlined"
-              size="small"
-              onClick={handleCheckedLeft}
-              disabled={rightChecked.length === 0}
-              aria-label="move selected left"
-            >
-              &lt;
-            </Button>
-          </Grid>
-        </Grid>
-        <Grid item>{customList('Chosen', right)}</Grid>
-      </Grid>
+      <div>
+        {tasks.map(value => (
+          <li>{value.title}</li>
+        ))}
+      </div>
       <div className="tasks-input-area">
         <div className="tasks-textfield">
-          <TextField id="outlined-search" label="Search field" type="search" variant="outlined" fullWidth="true" />
+          <TextField 
+            id="outlined-search" 
+            label="タスクを追加" 
+            type="text" 
+            variant="outlined" 
+            fullWidth="true"
+            value={taskName}
+            onChange={inputTaskName} 
+             />
         </div>
         <div className="tasks-add-button">
-          <Button variant="contained" size="large" color="primary" >
+          <Button 
+            variant="contained" 
+            size="large" 
+            color="primary"
+            onClick={addTask}
+             >
             追加
           </Button>
         </div>
       </div>
     </div>
-  );
+  )
 }
+
+export default Tasks;
